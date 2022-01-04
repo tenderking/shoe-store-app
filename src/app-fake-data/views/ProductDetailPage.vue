84c155c14bbbf51f4a006d602658e6e385cbb7cf
George Mushore <gmu003@gmail.com>
2022-01-04 14:57:50 +0100

    create option to view fake-api

---
+++ src/app-fake-data/views/ProductDetailPage.vue
@@ -0,0 +1,75 @@
+<template>
+  <div id="page-wrap" v-if="product">
+    <div id="img-wrap">
+      <img v-bind:src="product.imageUrl" />
+    </div>
+    <div id="product-details">
+      <h1>{{ product.name }}</h1>
+      <h3 id="price">${{ product.price }}</h3>
+      <p>Average rating: {{ product.averageRating }}</p>
+      <button v-if="!showSucessMessage" id="add-to-cart" v-on:click="addToCart">
+        Add to Cart
+      </button>
+      <button v-if="showSuccessMessage" id="add-to-cart" class="green-btn">
+        Successfully added item to cart!
+      </button>
+      <h4>Description</h4>
+      <p>{{ product.description }}</p>
+    </div>
+  </div>
+  <NotFoundPage v-else />
+</template>
+
+<script>
+import { products } from '../fake-data';
+
+import NotFoundPage from "./NotFoundPage";
+
+export default {
+  name: "ProductDetailPage",
+  components: {
+    NotFoundPage
+  },
+  data() {
+    return {
+      product: products.find((p) => p.id === this.$route.params.id),
+    };
+  },
+
+  
+};
+</script>
+
+<style scoped>
+#page-wrap {
+  margin-top: 16px;
+  padding: 16px;
+  max-width: 600px;
+}
+
+#img-wrap {
+  text-align: center;
+}
+
+img {
+  width: 400px;
+}
+
+#product-details {
+  padding: 16px;
+  position: relative;
+}
+
+#add-to-cart {
+  width: 100%;
+}
+
+#price {
+  position: absolute;
+  top: 24px;
+  right: 16px;
+}
+.green-btn {
+  background-color: green;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
+import App from "./app-fake-data/App.vue";
+import router from "./app-fake-data/router";
 
 createApp(App)
   .use(router)
