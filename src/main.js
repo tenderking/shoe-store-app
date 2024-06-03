080a809eb0335c81a064affcd7afc535a2bebe2b
George <gmu003@gmail.com>
2024-06-03 13:33:54 +0200

    chore: update to vue 3.4 with vite from webpack

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia"
+const pinia = createPinia()
 import App from "./app-fake-data/App.vue";
 import router from "./app-fake-data/router";
 
 createApp(App)
+.use(pinia)
   .use(router)
   .mount("#app");
